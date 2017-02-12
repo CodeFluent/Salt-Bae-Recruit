5d2b8a3c7a733a5d7982ff249ade2bbd15ee2871
CodeFluent <wasfimomenwim@gmail.com>
2017-02-12 04:43:30 -0500

    PDF in, view console for array buffer of pdf content

--- custom.js
+++ custom.js
@@ -1,1 +1,16 @@
 console.log("custom.js loaded!");
+
+var url = "http://cdn.mozilla.net/pdfjs/helloworld.pdf";
+
+
+PDFJS.getDocument(url).then(function getPdfHelloWorld(pdf) {
+
+    pdf.getData().then(function(arrayBuffer) {
+        var pdfraw = String.fromCharCode.apply(null, arrayBuffer);
+
+        // Operate on your raw pdf here...
+
+        console.log(pdfraw);
+
+    });
+});
